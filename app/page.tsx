b6923f4d7f87291894fc3e4106262335c4d4dd48
Connie <connieramirezphotography@gmail.com>
2025-06-11 23:36:49 +0100

    links to projects updated

--- app/page.tsx
+++ app/page.tsx
@@ -73,14 +73,14 @@ export default function Home() {;
         <h2>Projects</h2>
         <div className={styles.projectGrid}>
           <div className={styles.project}>
-            <Link href="https://www.figma.com/board/KStmAELGoiVoV5DzSUPtWX/BaggIt-APP-(detailed)?node-id=0-1&t=pNCvtnCgvnZl8md2-1" target="_blank"><h3> <Image src="/suitcase-64.png" alt="Suitcase icon" width={64} height={64} /> BaggIt App</h3></Link>
+            <Link href="https://baggit-app.vercel.app/home" target="_blank"><h3> <Image src="/suitcase-64.png" alt="Suitcase icon" width={64} height={64} /> BaggIt App</h3></Link>
             <p>
-            The primary challenge BaggIt addresses is the lack of a convenient, organized solution for travelers to store and manage their belongings on the go. On this project, I led the design and contributed to the front-end development, creating intuitive, user-friendly interface components that ensure a seamless user experience. I also developed the logo, brand identity, and all the visual assets used within the app.</p>
+            The primary challenge BaggIt addresses is the lack of a convenient, organised solution for travellers to store and manage their belongings on the go. On this project (see the process  <Link href="https://www.figma.com/board/KStmAELGoiVoV5DzSUPtWX/BaggIt-APP-(detailed)?node-id=0-1&t=pNCvtnCgvnZl8md2-1" target="_blank"><u>here</u></Link>), I led the design and contributed to the front-end development, creating intuitive, user-friendly interface components that ensure a seamless user experience. I also developed the logo, brand identity, and all the visual assets used within the app. For a summary, check <Link href="https://www.canva.com/design/DAGTvkGibmw/KAmHrgLba2rMl0N8s5CV5w/view?utm_content=DAGTvkGibmw&utm_campaign=designshare&utm_medium=link&utm_source=editor#1" target="_blank"><u>here</u>.</Link></p>
           </div>
           <div className={styles.project}>
-            <Link href="https://www.figma.com/board/GXsyW08KfIt33EWqtGOkiH/Weather-App-Process?node-id=0-1&t=PiTMoylOHIh6Z6oz-1" target="_blank"><h3> <Image src="/barometer-64.png" alt="Barometer icon" width={64} height={64} /> Weather App</h3> </Link>
+            <Link href="https://weather-lthn86kmi-connie-ramirezs-projects.vercel.app/" target="_blank"><h3> <Image src="/barometer-64.png" alt="Barometer icon" width={64} height={64} /> Weather App</h3> </Link>
             <p>
-            This simple, user-friendly tool allows you to quickly check the weather for any city. The development process behind the app involved a structured approach, starting with brainstorming various features and layouts to ensure the design was intuitive and easy to use. Feedback played a key role in this iteration process, helping us plan a more efficient and accessible tool.</p>
+            This simple, user-friendly tool allows you to quickly check the weather for any city. The development process behind the app involved a structured approach, starting with brainstorming various features and layouts to ensure the design was intuitive and easy to use (see the process  <Link href="https://www.figma.com/board/GXsyW08KfIt33EWqtGOkiH/Weather-App-Process?node-id=0-1" target="_blank"><u>here</u></Link>). Feedback played a key role in this iteration process, helping us plan a more efficient and accessible tool.To see the repo, click <Link href="https://github.com/Cecil2024/Weather-App/tree/main" target="_blank"><u>here</u>.</Link></p>
           </div>
           <div className={styles.project}>
             <Link href="https://www.figma.com/board/JRbX20xi3cogIFmaBYi3pA/User-Research-and-Ideation-Methods-for-LPA-App?node-id=0-1&t=E1eLVbVUFXJ0VzeF-1" target="_blank"><h3> <Image src="/letter-clinic-64.png" alt="Medical Letter icon" width={64} height={64} /> LPA Research and Ideation</h3></Link>
@@ -89,16 +89,16 @@ export default function Home() {;
           </div>
           <div className={styles.project}>
             <Link href="https://www.connieramphoto.co.uk/" target="_blank"><h3> <Image src="/camera-64.png" alt="Camera icon" width={64} height={64} /> Photographic website</h3></Link>
-            <p>A visually striking and user-centred photography portfolio designed and developed to highlight the artistic work of Connie Ramirez Photography. In addition to the website, I also crafted the entire branding and social content, ensuring a cohesive identity across all platforms. The website features a clean, minimalist layout with responsive design, intuitive navigation, and a focus on high-quality imagery. The UX/UI design ensures an immersive experience, allowing the photography to take centre stage while offering seamless interaction across devices.</p>
+            <p>A visually striking and user-centred photography portfolio designed and developed to highlight the artistic work of Connie Ramirez Photography, make bookings and contact the photographer. In addition to the website, I also crafted the entire branding and social content, ensuring a cohesive identity across all platforms. The website features a clean, minimalist layout with responsive design, intuitive navigation, and a focus on high-quality imagery. The UX/UI design ensures an immersive experience, allowing the photography to take centre stage while offering seamless interaction across devices.To see the repo, click <Link href="https://github.com/Cecil2024/photo" target="_blank"><u>here</u>.</Link></p>
           </div>
           <div className={styles.project}>
-            <Link href="https://www.figma.com/board/KqL6poiU120C3r5EFA2R89/Mrs.-Bee?node-id=0-1&t=VauDG2KrrUzVB7jY-1/" target="_blank"><h3> <Image src="/bee-64.png" alt="Bee icon" width={64} height={64} /> Website optimization</h3></Link>
-            <p>This board outlines the processes for optimizing and enhancing the website for "Mrs. Bee Coffee London." I began by identifying the strengths of the current website, then explore potential user personas. With the suggested improvements implemented, I designed a new website that features clean visuals and professional content, ensuring smooth navigation for users.</p>
+            <Link href="https://pruebapaginade.my.canva.site/sweet-sips-of-happiness" target="_blank"><h3> <Image src="/bee-64.png" alt="Bee icon" width={64} height={64} /> Website optimization</h3></Link>
+            <p>This board outlines the processes for optimizing and enhancing the website for "Mrs. Bee Coffee London." I began by identifying the strengths of the current website, then explore potential user personas. (See the process  <Link href="https://www.figma.com/board/KqL6poiU120C3r5EFA2R89/Mrs.-Bee?node-id=0-1&p=f&t=1WEU3xpSTPEgNoPP-0" target="_blank"><u>here</u></Link>) With the suggested improvements implemented, I designed a new website that features clean visuals and professional content, ensuring smooth navigation for users.</p>
           </div>
           <div className={styles.project}>
-            <Link href="https://github.com/Cecil2024/The-Know-It-ALL" target="_blank"><h3> <Image src="/book-64.png" alt="Book icon" width={64} height={64} /> The Know-it-all</h3></Link>
+            <Link href="https://v0-clone-this-project-plum.vercel.app/" target="_blank"><h3> <Image src="/book-64.png" alt="Book icon" width={64} height={64} /> The Know-it-all</h3></Link>
             <p>Welcome to an AI chatbot that holds the answers to all your questions! But what makes it truly special is that you can tailor its responses to suit your preferences—so go ahead and explore! You might just be amazed by what you discover!
-            In this project, I had the exciting opportunity to lead the UI and front-end development, crafting an engaging and intuitive user experience.</p>
+            In this project, I had the exciting opportunity to lead the UI and front-end development, crafting an engaging and intuitive user experience. To see the repo, click <Link href="https://github.com/Cecil2024/The-Know-It-ALL" target="_blank"><u>here</u>.</Link></p>
           </div>
         </div>
       </section>
